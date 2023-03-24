87f3b75b465f6cfa241c2bfefc0ade54ff35d7fe
CaritaNi <carita.niskanen@hotmail.com>
2023-03-24 13:08:53 +0200

    toimiva flex

--- GymApp/App.js
+++ GymApp/App.js
@@ -1,13 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './components/Home';
-import BottomNav from './components/BottomNav';
 import styles from './styles/styles';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Home />
-      <BottomNav />
     </View>
   );
 }
