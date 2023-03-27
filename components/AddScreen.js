2e7a8fdc14d355275bdef2e0efde568417e91678
Juho <juho.ahonen2@gmail.com>
2023-03-27 10:06:55 +0300

    async testing for adding execrices

--- components/AddScreen.js
+++ components/AddScreen.js
@@ -1,9 +1,52 @@
 import { View, Text } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useState, useEffect } from "react";
+import { Button } from "react-native";
+import { TextInput } from "react-native";
+
+
 
  const AddScreen = () => {
+    const [value, setValue] = useState("");
+    const [text, setText] = useState("");
+    const saveData = async (value) => {
+        try {
+            await AsyncStorage.setItem("test", JSON.stringify(value));
+          } catch (error) {
+            console.log(error);
+          }
+        };
+
+    const getData = async () => {
+        try {
+            const savedData = await AsyncStorage.getItem("test");
+            const currentData = JSON.parse(savedData);
+            console.log(currentData);
+            setText(currentData);
+
+        } catch (error) {
+            console.log(error);
+        }
+        };
+
+    function buttonPressed() {
+        saveData(value);
+        getData();  
+    }
+
+
+    
+
     return (
         <View>
             <Text>MORO</Text>
+            <TextInput
+                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                onChangeText={text => setValue(text)}
+                value={value}
+            />
+            <Button title="Save" onPress={() => {buttonPressed()}}/>
+            <Text>{text}</Text>
         </View>
     )
  }
