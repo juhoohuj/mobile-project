a822e4d9ad413eeffc61af250dd8799915a2101e
AtteTaajo <32447990+AtteTaajo@users.noreply.github.com>
2023-03-27 09:54:46 +0300

    asyncjuttui

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,10 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, useState } from "react-native";
 
+const [workOut, setWorkOuts] = useState(Array(20).fill('').map((_,i) => ('Test ${i}')));
 
  const HomeScreen = () => {
     return (
         <View>
-            <Text>MORO</Text>
+            <ScrollView>
+                {
+                workOut.map((workOut,index) => (
+                    <View key={index} style={StyleSheet.rowContainer}>
+                        <Text style={StyleSheet.rowText}>{workOut}</Text>
+                    </View>
+                ))
+                }
+            </ScrollView>
         </View>
     )
  }
