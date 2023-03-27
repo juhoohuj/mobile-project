ff4b95b6d66aa603e7866094ea8fbb8e5e0f4464
CaritaNi <carita.niskanen@hotmail.com>
2023-03-27 09:51:21 +0300

    uus branch

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,12 +1,20 @@
 import { View, Text } from "react-native";
+import React, { Component } from 'react';
+import styles from '../styles/Styles';
+
 
 
  const HomeScreen = () => {
     return (
-        <View>
-            <Text>MORO</Text>
+        <View style={styles.container}>
+        <Text style={styles.headerStyle}>Hi user</Text>
+        <View style={[{flex: 1}, styles.elementsContainer]}>
+          <View style={{flex: 2, backgroundColor: '#647AA3', borderRadius: 25 }} />
+          <View style={{flex: 2, backgroundColor: '#334195', borderRadius: 25}} />
+          <View style={{flex: 2, backgroundColor: '#020887', borderRadius: 25}} />
         </View>
-    )
+      </View>
+    );
  }
 
 
