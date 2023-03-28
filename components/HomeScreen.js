0986a2128f4e28eb3a641fe0b0a77ccbec17c933
AtteTaajo <32447990+AtteTaajo@users.noreply.github.com>
2023-03-28 12:25:08 +0300

    async fiksuailua

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,34 +1,12 @@
 import { View, Text, ScrollView, useState } from "react-native";
-<<<<<<< Updated upstream
-
-const [workOut, setWorkOuts] = useState(Array(20).fill('').map((_,i) => ('Test ${i}')));
-=======
 import React from 'react';
->>>>>>> Stashed changes
 
  const HomeScreen = () => {
 
-    
-    const [workOuts, setWorkOuts] = useState(
-        Array(20).fill('').map((_,i) => ('Test ${i}')));
 
     return (
         <View>
-            <ScrollView>
-                {
-<<<<<<< Updated upstream
-                workOut.map((workOut,index) => (
-                    <View key={index} style={StyleSheet.rowContainer}>
-                        <Text style={StyleSheet.rowText}>{workOut}</Text>
-=======
-                workOuts.map((workOut,index) => (
-                    <View key={index}>
-                        <Text>{workOut}</Text>
->>>>>>> Stashed changes
-                    </View>
-                ))
-                }
-            </ScrollView>
+            <Text>pöö</Text>
         </View>
     )
 }
