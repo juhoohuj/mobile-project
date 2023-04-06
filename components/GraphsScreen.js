c5df9717f760d1157a55cd2361b761250dccfb05
Louhela <niko.louhela@gmail.com>
2023-04-06 10:07:47 +0300

    Fixed crashing bug on graph, added buttons to scroll graph

--- components/GraphsScreen.js
+++ components/GraphsScreen.js
@@ -3,11 +3,26 @@ import { ScrollView } from "react-native";
 import ScrollGraphProps from "../components/ScrollGraphProps"
 
 const GraphsScreen = () => {
+
+
+// let filledArray2 = new Array(400).fill(70);
+
+const n = 71;
+let filledArray = new Array(n).fill("30.3.2023");
+const arrayN = Array(n).fill('n').map((v,i)=>i+1);
+console.log(arrayN)
+
+
+console.log(arrayN.length)
+console.log(filledArray.length)
+// console.log(filledArray)
+// console.log(filledArray2)
     return (
         <ScrollView>
-            <ScrollGraphProps GraphName={"Weight"} DataUnitsType="kg" dataPointDates={[]} mainDataPoints={[]}/>
-            <ScrollGraphProps GraphName={"Joku1"} DataUnitsType="cm" dataPointDates={["30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023","30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023",]} mainDataPoints={[60, 61, 62, 60, 60, 61, 62, 60, 60, 61, 62, 60, ]}/>
-            <ScrollGraphProps GraphName={"Joku2"} DataUnitsType="kg" dataPointDates={["30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023","30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023",]} mainDataPoints={[60, 61, 62, 60, 60, 61, 62, 60, 60, 61, 62, 60, ]}/>
+            <ScrollGraphProps GraphName={"Weight"} DataUnitsType="kg" dataPointDates={filledArray} mainDataPoints={arrayN}/>
+            {/* <ScrollGraphProps GraphName={"Weight"} DataUnitsType="kg" dataPointDates={["30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023","30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023","30.3.2023","30.3.2023","30.3.2023",]} mainDataPoints={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, ]}/> */}
+            {/* <ScrollGraphProps GraphName={"Joku1"} DataUnitsType="cm" dataPointDates={["30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023","30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023",]} mainDataPoints={[60, 61, 62, 60, 60, 61, 62, 60, 60, 61, 62, 60, ]}/> */}
+            {/* <ScrollGraphProps GraphName={"Joku2"} DataUnitsType="kg" dataPointDates={["30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023","30.3.2023", "30.3.2023", "30.3.2023", "30.3.2023",]} mainDataPoints={[60, 61, 62, 60, 60, 61, 62, 60, 60, 61, 62, 60, ]}/> */}
         </ScrollView>
     )
  }
--- components/ScrollGraphProps.js
+++ components/ScrollGraphProps.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View, Dimensions, TextInput, Button, ScrollView, Pres
 import styles from "../styles/Styles";
 import { useState } from 'react';
 import { LineChart } from "react-native-chart-kit";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, AntDesign } from '@expo/vector-icons';
 
 export default function ScrollGraphProps({dataPointDates, DataUnitsType, mainDataPoints, GraphName}){
 
@@ -15,7 +15,90 @@ const [SelctedDataPointDate, setSelctedDataPointDate] = useState("--.--.----")
 const [DataPointDate, setDataPointDate] = useState([...dataPointDates])
 const [mainDataPoint, setMainDataPoint] = useState([...mainDataPoints])
 
+
+const itemsPerPage = 7;
+const [currentIndex, setCurrentIndex] = useState(mainDataPoint.length - itemsPerPage);
+
+
+// let mainDataPoint = mainDataPoints
+// let DataPointDate = dataPointDates
+
+// let rangeStart = -14
+// let rangeEnd = -7
+
 const [graphWidth, setGraphWidth] = useState(mainDataPoint.length > 3 ? Dimensions.get("window").width - 20 + (100 * mainDataPoint.length) - 400: Dimensions.get("window").width - 20)
+
+// const [DataRange, setDataRange] = useState(mainDataPoint.slice(-7))
+// const [DateRange, setDateRange] = useState(DataPointDate.slice(-7))
+
+// const [rangeStart, setrangeStart] = useState(-7)
+// const [rangeEnd, setrangeEnd] = useState(0)
+
+
+
+// console.log(mainDataPoint)
+
+
+// console.log(DataRange)
+// console.log(DateRange)
+
+// function changeRange(direction) {
+
+//   console.log(rangeStart, rangeEnd)
+//   // console.log(direction)
+//   if (direction == "back"){
+//     setrangeEnd(rangeEnd -7)
+//     setrangeStart(rangeStart -7)
+
+//     setDateRange(DataPointDate.slice(rangeStart, rangeEnd))
+//     setDataRange(mainDataPoint.slice(rangeStart, rangeEnd))
+
+//     // rangeStart = rangeStart -7
+//     // rangeEnd = rangeEnd -7
+//     console.log(rangeStart, rangeEnd)
+    
+    
+//   }
+//   else {
+//     setrangeEnd(rangeEnd +7)
+//     setrangeStart(rangeStart +7)
+
+//     setDateRange(DataPointDate.slice(rangeStart, rangeEnd))
+//     setDataRange(mainDataPoint.slice(rangeStart, rangeEnd))
+
+//     // rangeStart = rangeStart +7
+//     // rangeEnd = rangeEnd +7
+//     console.log(rangeStart, rangeEnd)
+
+//   }
+
+// }
+
+
+const handleRangeButtonPress = (increment) => {
+  const newIndex = currentIndex + increment;
+  
+  console.log(newIndex, currentIndex);
+  
+  if (newIndex < 0) {
+    console.log('setting newIndex to 0');
+    setCurrentIndex(0);
+  } else if (newIndex > mainDataPoint.length - itemsPerPage) {
+    console.log('setting newIndex to max');
+    setCurrentIndex(mainDataPoint.length - itemsPerPage);
+  } else {
+    setCurrentIndex(newIndex);
+  }
+};
+
+const isFirstPage = currentIndex === 0;
+const isLastPage = currentIndex === mainDataPoint.length - itemsPerPage;
+
+
+const displayedItems = mainDataPoint.slice(currentIndex, currentIndex + itemsPerPage);
+const displayedItems2 = DataPointDate.slice(currentIndex, currentIndex + itemsPerPage);
+
+console.log(displayedItems)
 
 
 function handleAddWeight(){
@@ -23,28 +106,29 @@ function handleAddWeight(){
     setDataPointDate([...DataPointDate, currentDate.toLocaleDateString()])
     setMainDataPoint([...mainDataPoint, parseFloat(inputWeight)])
 
+  
     if(mainDataPoint.length > 3) {
         setGraphWidth(graphWidth + 100)
     }
   }
 
 
-  if (mainDataPoint.length == 0){
-    return (
-      <View style={styles.graphContainer}>
-        <Text style={styles.graphName}>No data</Text>
-        <TextInput
-          style={styles.graphInput}
-          value={inputWeight.toString()}
-          onChangeText={value => setInputWeight(value)}
-          placeholder="Input your weight"
-          keyboardType="numeric"/>
-          <Pressable onPress={handleAddWeight}>
-              <Ionicons name="add-circle-sharp" size={54} color="white" />
-          </Pressable>
-      </View>
-    )
-  }
+  // if (mainDataPoint.length == 0){
+  //   return (
+  //     <View style={styles.graphContainer}>
+  //       <Text style={styles.graphName}>No data</Text>
+  //       <TextInput
+  //         style={styles.graphInput}
+  //         value={inputWeight.toString()}
+  //         onChangeText={value => setInputWeight(value)}
+  //         placeholder="Input your weight"
+  //         keyboardType="numeric"/>
+  //         <Pressable onPress={handleAddWeight}>
+  //             <Ionicons name="add-circle-sharp" size={54} color="white" />
+  //         </Pressable>
+  //     </View>
+  //   )
+  // }
 
     return (
         <View style={styles.graphContainer}>
@@ -62,14 +146,16 @@ function handleAddWeight(){
             // Auto scroll to end when new measurement is added
             ref={ref => {this.scrollView = ref}}
             onContentSizeChange={() => this.scrollView.scrollToEnd({animated: true})}>
-                <LineChart
+                  <LineChart
                     data={{
-                    labels: DataPointDate,
+                    labels: displayedItems2,
                     datasets: [
                         {
-                        data: mainDataPoint
-                        }]}}
-                    width={graphWidth}
+                        data: displayedItems
+                        }
+                    ]
+                    }}
+                    width={Dimensions.get("window").width - 20}
                     height={220}
                     yAxisLabel=""
                     yAxisSuffix={DataUnitsType}
@@ -84,6 +170,13 @@ function handleAddWeight(){
                     style: {
                         borderRadius: 16
                     },
+                    propsForVerticalLabels:{
+                      // fontSize: "14",
+                      rotation: 25,
+                      // alignmentBaseline: "ideographic"
+                      // disabled: "true"
+
+                    },
                     propsForDots: {
                         r: "6",
                         strokeWidth: "2",
@@ -95,18 +188,37 @@ function handleAddWeight(){
                     marginVertical: 8,
                     borderRadius: 16,
                     }}
+                    
                     onDataPointClick={({value, index, getColor}) => {
                     console.log("Weight: " + value + "kg" + " Date: " + DataPointDate[index])
                     setSelectedDataPoint(value + DataUnitsType)
                     setSelctedDataPointDate(DataPointDate[index])
                     }}/>
                 </ScrollView>
+                <View style={{flexDirection: "row", width: Dimensions.get("window").width - 30, justifyContent: "space-between", alignItems: "flex-start"}}>
+
+
+                  <Pressable onPress={() => handleRangeButtonPress(-itemsPerPage)}>
+                    <AntDesign name="arrowleft" size={54} color={isFirstPage ? "gray" : "white"} />
+                  </Pressable>
+
+
+
             <TextInput
               style={styles.graphInput}
               value={inputWeight.toString()}
               onChangeText={value => setInputWeight(value)}
               placeholder="Input your weight"
               keyboardType="numeric"/>
+
+
+
+                <Pressable onPress={() => handleRangeButtonPress(itemsPerPage)}>
+                  <AntDesign name="arrowright" size={54} color={isLastPage ? "gray" : "white"} />
+                </Pressable> 
+                </View>
+
+
             <Pressable onPress={handleAddWeight}>
               <Ionicons style={styles.graphButton} name="add-circle-sharp" size={54} color="white" />
             </Pressable>
