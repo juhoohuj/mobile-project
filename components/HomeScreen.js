2bfceae609d95879b71bbe8113cc2620e61e90f3
AtteTaajo <atte.taajoranta@gmail.com>
2023-04-11 00:19:44 +0300

    workoutit etusivulle

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -8,78 +8,43 @@ const STORAGE_KEY = '@exe-Key';
 
 const HomeScreen = () => {
     const [exes, setExes] = useState([]);
-    const [value, setValue] = useState("");
     
     const [text, setText] = useState("");
-    /*const [todos, setTodos] = useState(
-        Array(20).fill('').map((_,i) => (`Test ${i}`))
-    );*/
-    
+   
 
-      const storeData = async (value) => {
-        console.log(value, "valueeeeee");
+            
+    const getWorkouts = async () => {
         try {
-            const jsonValue = JSON.stringify(value);
-            await AsyncStorage.setItem(STORAGE_KEY, jsonValue);
-            //await AsyncStorage.setItem('key',JSON.stringify(value));
-        } catch (e){
-            console.log("store", e);
-        }
-      }
-        /*return AsyncStorage.getItem(STORAGE_KEY)
-                    .then (req => JSON.parse(req))
-                    .then (json => {
-                        console.log(req);
-                        if (json === null) {
-                            json = [];
-                            
-                        }
-                        setExes(json);
-                    })
-                    .catch (error => console.log("get1",error));*/
-            //****getData vanhoja juttuja****
-            //const savedValue = await AsyncStorage.getItem('key');
-            //const curValue = JSON.parse(savedValue);
-            //setText(curValue);
+            return AsyncStorage.getItem('workouts')
+            .then (req => JSON.parse(req))
+            .then (json => {
+                if (json === null) {
+                    json = [];
+                }
+                console.log(json);
+                setExes(json);
+            })
+            .catch (error => console.log(error));
 
-    const getData = async() => {
-        
-        try {
-            const savedValue = await AsyncStorage.getItem(STORAGE_KEY);
-            const curValue = JSON.parse(savedValue);
-            setValue(curValue);
-        } catch (e) {
-            console.log("get", e);
-        }
-    }
+            } catch (e) {
+                console.error(e);
+            }
+    };
 
     useEffect(() => {
-        console.log(text);
         
-        /*if (text === "") {
-            console.log("ifi sisäl");
-            const newKey = exes.lenght + 1;
-            const newExe = {key: newKey.toString(), description: text};
-            const newExes = [...exes, newExe];
-
-            storeData(newExes);
-        }*/
-        
+        console.log(exes);
+       
     })
 
     function buttonPressed() {
 
-        
-        console.log("ifi sisäl");
         /*const newKey = exes.lenght + 1;
         const newExe = {key: newKey.toString(), description: text};
         const newExes = [...exes, newExe];*/
 
-        console.log("ene storee");
-        storeData(text);
-        console.log("jälkee storee");
 
-        getData();
+        getWorkouts();
     }
 
 
@@ -87,12 +52,17 @@ const HomeScreen = () => {
         <View>
             <Text>Test</Text>
 
-            <TextInput style={{ height: 40, borderColor: 'black', borderWidth: 1 }} onChangeText={setText} value={text}
-            />
 
             <Button title="Test" onPress={() => {buttonPressed()}}/>
+
+        {
+            exes.map((exe) => (
+                <View>
+                    <Text>{exe.name} </Text>
+                </View>
+            ))
+        }            
             
-            <Text>{value}</Text>
             
         </View>
     )
