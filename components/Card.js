44b765b0b66e1288907ff00e9f3ce61732285b23
CaritaNi <93704532+CaritaNi@users.noreply.github.com>
2023-05-03 17:07:38 +0300

    Added text shadow to cards

--- components/Card.js
+++ components/Card.js
@@ -5,9 +5,28 @@ import styles from "../styles/Styles";
 
 export default function Card({ navigation, cardText, destination, icon }) {
     return(
-        <TouchableOpacity style={{flexDirection: "row",  backgroundColor: '#0d2863', borderRadius: 25, borderColor: '#334195', borderWidth: 2, height: 150, width: 190, alignItems: "center", justifyContent: "space-evenly",  marginBottom: 5, marginTop: 15}}  onPress={() => navigation.navigate(destination)}>
+        <TouchableOpacity style={{
+          flexDirection: "row",  
+          backgroundColor: '#0d2863', 
+          borderRadius: 25, borderColor: '#334195', 
+          borderWidth: 2, 
+          height: 150, 
+          width: 190, 
+          alignItems: "center", 
+          justifyContent: "space-evenly",  
+          marginBottom: 5, 
+          marginTop: 15,
+          textShadowColor: 'rgba(0, 0, 0, 1)',
+            textShadowOffset: {width: -1, height: 1},
+            textShadowRadius: 15}}  
+          onPress={() => navigation.navigate(destination)}>
           <Text style={styles.profileText}>{cardText}</Text>
-        	<Entypo name={icon} size={48} color="white" />
+        	<Entypo name={icon} 
+            size={48} style={{
+            color:"white",
+            textShadowColor: 'rgba(0, 0, 0, 1)',
+            textShadowOffset: {width: -1, height: 1},
+            textShadowRadius: 10 }}/>
         </TouchableOpacity>
     )
 }
