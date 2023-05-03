f1e7bde72f523b07b85edbee114ed630418ee841
Dolez777 <73564898+Dolez777@users.noreply.github.com>
2023-05-03 16:08:19 +0300

    profilestack.js addscreen navigaatio

--- components/ProfileStack.js
+++ components/ProfileStack.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { GraphsScreen } from './GraphsScreen'
 import { ProfileScreen } from "./ProfileScreen";
 import { CreateGraphScreen } from "./CreateGraphScreen";
+import { AddScreen } from "./AddScreen";
 
 
 const Stack = createNativeStackNavigator();
@@ -13,6 +14,7 @@ function ProfileStack() {
             <Stack.Screen name="MainProfileScreen" component={ProfileScreen} options={{headerShown: false}}/>
             <Stack.Screen name="Graphs" component={GraphsScreen} />
             <Stack.Screen name="CreateGraphs" component={CreateGraphScreen} options={{ headerTitle: 'My Graphs'}} />
+            <Stack.Screen name="AddScreen" component={AddScreen} options={{headerShown: false}}/>
         </Stack.Navigator>
     );
   }
