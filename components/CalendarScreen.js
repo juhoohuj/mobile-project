cb602d7665fdd41ec9670e6136692aa023254dfe
CaritaNi <93704532+CaritaNi@users.noreply.github.com>
2023-05-03 16:54:27 +0300

    Calendar safearea

--- components/CalendarScreen.js
+++ components/CalendarScreen.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, SafeAreaView } from "react-native";
 import styles from "../styles/Styles";
 import WorkoutCalendar from "./Calendar";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
 
 const CalendarScreen = ({ navigation }) => {
-    return (
-      <View style={styles.container}>
+  const insets = useSafeAreaInsets();
+
+  return (
+    <SafeAreaView style={[styles.container, { paddingTop: insets.top }]}>
+      <View>
         <Text style={styles.headerStyle}>CALENDAR</Text>
         <WorkoutCalendar />
-        {/* <Text>asdadsads</Text> */}
       </View>
-    )
- }
+    </SafeAreaView>
+  );
+};
 
 
 export {CalendarScreen}
