e0c5dd66f9a4237a41a6ce9cbfa4925e4744dd48
Dolez777 <73564898+Dolez777@users.noreply.github.com>
2023-05-03 16:31:05 +0300

    Koodin siivoamista

--- components/ProfileStack.js
+++ components/ProfileStack.js
@@ -4,19 +4,24 @@ import { GraphsScreen } from './GraphsScreen'
 import { ProfileScreen } from "./ProfileScreen";
 import { CreateGraphScreen } from "./CreateGraphScreen";
 import { AddScreen } from "./AddScreen";
-
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Stack = createNativeStackNavigator();
 
 function ProfileStack() {
     return (
-        <Stack.Navigator>
-            <Stack.Screen name="MainProfileScreen" component={ProfileScreen} options={{headerShown: false}}/>
+        
+
+        <Stack.Navigator
+        screenOptions={{
+            headerShown: false,}}
+  >
+            <Stack.Screen name="MainProfileScreen" component={ProfileScreen} />
             <Stack.Screen name="Graphs" component={GraphsScreen} 
-            options={{headerShown: false}}/>
-            <Stack.Screen name="CreateGraphs" component={CreateGraphScreen} options={{headerShown: false}}
+          />
+            <Stack.Screen name="CreateGraphs" component={CreateGraphScreen} 
              />
-            <Stack.Screen name="AddScreen" component={AddScreen} options={{headerShown: false}}/>
+            <Stack.Screen name="AddScreen" component={AddScreen} />
         </Stack.Navigator>
     );
   }
